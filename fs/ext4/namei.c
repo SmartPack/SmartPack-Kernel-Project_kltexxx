f9eff6bf6af51928adb35d4b5e0bb06e5f6fb9c7
Theodore Ts'o <tytso@mit.edu>
2014-09-03 09:33:00 -0400

    ext4: avoid trying to kfree an ERR_PTR pointer
    commit a9cfcd63e8d206ce4235c355d857c4fbdf0f4587 upstream.
    
    Thanks to Dan Carpenter for extending smatch to find bugs like this.
    (This was found using a development version of smatch.)
    
    Fixes: 36de928641ee48b2078d3fe9514242aaa2f92013
    Reported-by: Dan Carpenter <dan.carpenter@oracle.com
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    [lizf: Backported to 3.4:
    - s/new.bh/new_bh/
    - drop the change to ext4_cross_rename()]
    Signed-off-by: Zefan Li <lizefan@huawei.com>

--- fs/ext4/namei.c
+++ fs/ext4/namei.c
@@ -2567,6 +2567,7 @@ static int ext4_rename(struct inode *old_dir, struct dentry *old_dentry,
 #endif
 	if (IS_ERR(new_bh)) {
 		retval = PTR_ERR(new_bh);
+		new_bh = NULL;
 		goto end_rename;
 	}
 	if (new_bh) {
