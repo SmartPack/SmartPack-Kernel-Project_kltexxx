beecb126ddfcb922f831ec5b00f4f88de057d3de
Gobinda Joy <gobinda.joy@gmail.com>
2017-06-27 20:37:31 +0600

    build: Fix section mismatch
    # Conflicts:
    #	arch/arm/mach-msm/msm-pm.c

--- arch/arm/mach-msm/mpm-of.c
+++ arch/arm/mach-msm/mpm-of.c
@@ -102,7 +102,7 @@ enum mpm_reg_offsets {
 	MSM_MPM_REG_STATUS,
 };
 
-static __refdata DEFINE_SPINLOCK(msm_mpm_lock);
+static DEFINE_SPINLOCK(msm_mpm_lock);
 
 static uint32_t msm_mpm_enabled_irq[MSM_MPM_REG_WIDTH];
 static uint32_t msm_mpm_wake_irq[MSM_MPM_REG_WIDTH];
@@ -117,7 +117,7 @@ enum {
 	MSM_MPM_DEBUG_NON_DETECTABLE_IRQ_IDLE = BIT(3),
 };
 
-static int msm_mpm_debug_mask __refdata = 1;
+static int msm_mpm_debug_mask = 1;
 module_param_named(
 	debug_mask, msm_mpm_debug_mask, int, S_IRUGO | S_IWUSR | S_IWGRP
 );
