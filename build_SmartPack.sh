0082363642b5cc98499a93cc2e897667c0796e18
sunilpaulmathew <sunil.kde@gmail.com>
2017-08-17 10:03:03 +0900

    Release tag: stable-v9_r1
    Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

--- build_SmartPack.sh
+++ build_SmartPack.sh
@@ -11,7 +11,7 @@ echo -e $COLOR_GREEN"\n (c) sunilpaulmathew@xda-developers.com\n"$COLOR_NEUTRAL
 TOOLCHAIN="/home/sunil/arm-linux-androideabi-4.9-linaro/bin/arm-linux-androideabi-"
 ARCHITECTURE=arm
 
-KERNEL_VERSION="stable"   # leave as such, if no specific version tag
+KERNEL_VERSION="stable-v9_r1"   # leave as such, if no specific version tag
 
 NUM_CPUS=""   # number of cpu cores used for build (leave empty for auto detection)
 
