f902ab4781fa39bf30529003b63a2a27cfd1e4db
sunilpaulmathew <sunil.kde@gmail.com>
2017-09-23 10:01:04 +0900

    First commit: Importing Samsung-oscr (G900FXXU1CQE1)
    Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

---
+++ anykernel_SmartPack/anykernel.sh
@@ -0,0 +1,65 @@
+#
+# SmartPack-Kernel (AnyKernel) Script for aosp Roms
+#
+# Credits: osm0sis @ xda-developers
+#
+# Modified by sunilpaulmathew@xda-developers.com
+#
+
+## AnyKernel setup
+# begin properties
+properties() {
+kernel.string=SmartPack Kernel by sunilpaulmathew@xda-developers.com
+do.devicecheck=1
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=kltexx
+device.name2=kltelra
+device.name3=kltetmo
+device.name4=kltecan
+device.name5=klteatt
+device.name6=klteub
+device.name7=klteacg
+device.name8=klte
+device.name9=kltekor
+device.name10=klteskt
+device.name11=kltektt
+device.name12=kltelgt
+device.name13=
+device.name14=
+device.name15=
+} # end properties
+
+# shell variables
+block=/dev/block/platform/msm_sdcc.1/by-name/boot;
+is_slot_device=0;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. /tmp/anykernel/tools/ak2-core.sh;
+
+
+## AnyKernel permissions
+# set permissions for included ramdisk files
+
+## AnyKernel install
+dump_boot;
+
+# begin ramdisk changes
+
+# init.rc
+
+# init.tuna.rc
+
+# init.superuser.rc
+
+# fstab.tuna
+
+# end ramdisk changes
+
+write_boot;
+
+## end install
+
