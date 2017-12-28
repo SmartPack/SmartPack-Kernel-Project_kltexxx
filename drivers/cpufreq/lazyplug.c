520540fa7b756c52d7d4dd22e83b8a6e9304d7b1
sunilpaulmathew <sunil.kde@gmail.com>
2017-12-28 10:19:06 +0900

    lazyplug: cleaning the code a bit...
    Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

--- drivers/cpufreq/lazyplug.c
+++ drivers/cpufreq/lazyplug.c
@@ -244,7 +244,6 @@ static void __ref cpu_all_ctrl(bool online) {
 
 static unsigned int calculate_thread_stats(void)
 {
-	unsigned int avg_nr_run = avg_nr_running();
 	unsigned int nr_run;
 	unsigned int threshold_size;
 	unsigned int *current_profile;
@@ -272,8 +271,6 @@ static unsigned int calculate_thread_stats(void)
 
 		if (nr_run_last <= nr_run)
 			nr_threshold += nr_run_hysteresis;
-		if (avg_nr_run <= (nr_threshold << (FSHIFT - nr_fshift)))
-			break;
 	}
 	nr_run_last = nr_run;
 
@@ -299,7 +296,6 @@ static void update_per_cpu_stat(void)
 
 	for_each_online_cpu(cpu) {
 		l_ip_info = &per_cpu(ip_info, cpu);
-		l_ip_info->cpu_nr_running = avg_cpu_nr_running(cpu);
 #ifdef DEBUG_LAZYPLUG
 		pr_info("cpu %u nr_running => %lu\n", cpu,
 			l_ip_info->cpu_nr_running);
