28e81b8297cdc99502d50d19b92050f860cb5a4d
sunilpaulmathew <sunil.kde@gmail.com>
2018-01-01 16:56:36 +0900

    build_SmartPack: adding new build framework for SmartPack-Kernel
    Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

---
+++ anykernel_SmartPack/anykernel.sh
@@ -0,0 +1,66 @@
+#
+# SmartPack-Kernel (AnyKernel) Script
+#
+# Credits: osm0sis @ xda-developers
+#
+# Modified by sunilpaulmathew@xda-developers.com
+#
+
+## AnyKernel setup
+# begin properties
+properties() {
+kernel.string=SmartPack Kernel by sunilpaulmathew@xda-developers.com
+do.devicecheck=1
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=kltexx
+device.name2=kltelra
+device.name3=kltetmo
+device.name4=kltecan
+device.name5=klteatt
+device.name6=klteub
+device.name7=klteacg
+device.name8=klte
+device.name9=kltekor
+device.name10=klteskt
+device.name11=kltektt
+device.name12=kltelgt
+device.name13=kltejpn
+device.name14=kltekdi
+device.name15=
+} # end properties
+
+# shell variables
+block=/dev/block/platform/msm_sdcc.1/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. /tmp/anykernel/tools/ak2-core.sh;
+
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+
+## AnyKernel install
+dump_boot;
+
+# begin ramdisk changes
+
+# init.rc
+
+# init.tuna.rc
+
+# init.superuser.rc
+
+# fstab.tuna
+
+# end ramdisk changes
+
+write_boot;
+
+## end install
+
