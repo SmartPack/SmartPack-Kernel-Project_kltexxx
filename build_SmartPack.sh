98892bb5c6b3b07fe895c8c4951d38ef0234f28e
sunilpaulmathew <sunil.kde@gmail.com>
2018-01-13 14:43:13 +0900

    build_SmartPack: removing some simple typo's from build script
    Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

--- build_SmartPack.sh
+++ build_SmartPack.sh
@@ -8,14 +8,14 @@
 
 #
 # This script is licensed under the terms of the GNU General Public 
-# License version 2, as published # by the Free Software Foundation, 
+# License version 2, as published by the Free Software Foundation, 
 # and may be copied, distributed, and modified under those terms.
 #
 
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR # PURPOSE. See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 #
 
