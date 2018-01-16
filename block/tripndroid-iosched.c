b2f078e5c7a48bc4f86b5e8218116a28ac40a8e6
gsstudios <josh.lay@exemail.com.au>
2018-01-17 09:41:28 +1100

    io-sched: increase fifo-batch to 16 to reduce eMMC load

--- block/tripndroid-iosched.c
+++ block/tripndroid-iosched.c
@@ -30,7 +30,7 @@ static const int async_read_expire  =  2 * HZ;	/* ditto for async, these limits 
 static const int async_write_expire = 2 * HZ;	/* ditto for async, these limits are SOFT! */
 
 static const int writes_starved = 1;		/* max times reads can starve a write */
-static const int fifo_batch     = 1;		/* # of sequential requests treated as one
+static const int fifo_batch     = 16;		/* # of sequential requests treated as one
 						   by the above parameters. For throughput. */
 
 struct tripndroid_data {
