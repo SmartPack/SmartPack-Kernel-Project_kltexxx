6e8cf41226d4f2af8c61412de0bcb6ed104f9013
sunilpaulmathew <sunil.kde@gmail.com>
2018-01-18 13:57:00 +0900

    ramdisk: Importing *SmartPack* boot script It can be utilized for activating kernel features while booting the device. The one feature incorporated into this script at this moment is "cpu_input_boost" by @sultanxda which is natively not accessible in KA/SPKM. Big thanks to @gsstudios for pointing this issue...
    Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

---
+++ anykernel_SmartPack/ramdisk/init.SmartPack.sh
@@ -0,0 +1,34 @@
+#!/system/bin/sh
+#
+#
+# SmartPack-Kernel Boot Script
+# 
+# Author: sunilpaulmathew <sunil.kde@gmail.com>
+#
+
+#
+# This script is licensed under the terms of the GNU General Public 
+# License version 2, as published by the Free Software Foundation, 
+# and may be copied, distributed, and modified under those terms.
+#
+
+#
+# This script is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+
+echo "Executing SmartPack Boot Script" | tee /dev/kmsg
+
+#
+# CPU Input Boost
+#
+echo 1190400 1497600 > /sys/kernel/cpu_input_boost/ib_freqs
+echo 1400 > /sys/kernel/cpu_input_boost/ib_duration_ms
+echo 1 > /sys/kernel/cpu_input_boost/enabled
+
+#
+# Done!
+#
+echo "Everything done" | tee /dev/kmsg
