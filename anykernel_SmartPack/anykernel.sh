9abe79cc64fe41d50a169395e5fa3c5706e3fc7e
sunilpaulmathew <sunil.kde@gmail.com>
2018-01-25 10:31:10 +0900

    ramdisk: Importing *SmartPack* boot script
    It can be utilized for activating kernel features while booting the device.
    The one feature incorporated into this script at this moment is "cpu_input_boost" by @sultanxda which is natively not accessible in KA/SPKM.
    Big thanks to @gsstudios for pointing this issue...
    
    Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

--- anykernel_SmartPack/anykernel.sh
+++ anykernel_SmartPack/anykernel.sh
@@ -44,6 +44,7 @@ ramdisk_compression=auto;
 
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
+chmod -R 755 $ramdisk
 
 ## AnyKernel install
 dump_boot;
@@ -56,6 +57,8 @@ insert_line default.prop "ro.config.tima=0" after "persist.sys.usb.config" "ro.c
 insert_line default.prop "ro.securestorage.support=false" after "persist.sys.usb.config" "ro.securestorage.support=false";
 
 # init.rc
+backup_file init.rc;
+grep "import /init.SmartPack.rc" init.rc >/dev/null || sed -i '1,/.*import.*/s/.*import.*/import \/init.SmartPack.rc\n&/' init.rc
 
 # init.tuna.rc
 
