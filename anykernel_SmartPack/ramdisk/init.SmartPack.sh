a6b38f4ab7f059d4bee02a4f3d78a84ec0278765
sunilpaulmathew <sunil.kde@gmail.com>
2018-02-22 12:25:31 +0900

    SmartPack boot script: reverting intelli thermal related stuff leaving it to users
    Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

--- anykernel_SmartPack/ramdisk/init.SmartPack.sh
+++ anykernel_SmartPack/ramdisk/init.SmartPack.sh
@@ -45,13 +45,6 @@ echo 1 > /sys/kernel/fast_charge/force_fast_charge
 echo 1 > /sys/module/adreno_idler/parameters/adreno_idler_active
 
 #
-# Enable intelli_thermal
-#
-echo 0 > /sys/module/msm_thermal/vdd_restriction/enabled
-echo 0 > /sys/module/msm_thermal/core_control/enabled
-echo Y > /sys/module/msm_thermal/parameters/enabled
-
-#
 # Done!
 #
 echo "Everything done" | tee /dev/kmsg
