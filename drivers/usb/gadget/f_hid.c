8fe7764dfc5eaee73c2f7563a6577b03a19f095a
sunilpaulmathew <sunil.kde@gmail.com>
2018-03-02 09:43:25 +0900

    gadget: some experiemntal changes to f_hid to avoid building error
    Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

--- drivers/usb/gadget/f_hid.c
+++ drivers/usb/gadget/f_hid.c
@@ -573,8 +573,8 @@ static int hidg_bind(struct usb_configuration *c, struct usb_function *f)
 
 	hidg_hs_in_ep_desc.bEndpointAddress =
 		hidg_fs_in_ep_desc.bEndpointAddress;
-	hidg_hs_out_ep_desc.bEndpointAddress =
-		hidg_fs_out_ep_desc.bEndpointAddress;
+	hidg_hs_in_ep_desc.bEndpointAddress =
+		hidg_hs_in_ep_desc.bEndpointAddress;
 
 	status = usb_assign_descriptors(f, hidg_fs_descriptors,
 			hidg_hs_descriptors, NULL);
