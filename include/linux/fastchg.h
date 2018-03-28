399d6009779473c6afc5bed78cea68d4af64cee0
sunilpaulmathew <sunil.kde@gmail.com>
2018-03-28 20:01:58 +0900

    mach-msm: update fast charge to v2.0 It includes the following changes... * Enabled Custom Mode by default * Bump USB and Wireless levels upto 1600 * Set default AC: 2000; USB:700; Wireless:900 * Reduced lower charge current for USB (400) and Wireless (600) * Miscellaneous changes
    Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

--- include/linux/fastchg.h
+++ include/linux/fastchg.h
@@ -3,6 +3,8 @@
  *
  * Ported to Note 3 (n9005) and extended : Jean-Pierre Rasquin <yank555.lu@gmail.com>
  *
+ * Updated: sunilpaulmathew <sunil.kde@gmail.com>
+ *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
  * may be copied, distributed, and modified under those terms.
@@ -18,17 +20,17 @@
 #ifndef _LINUX_FASTCHG_H
 #define _LINUX_FASTCHG_H
 
-#define FAST_CHARGE_VERSION "v1.2 by Yank555.lu"
+#define FAST_CHARGE_VERSION "v2.0"
 #define AC_LEVELS  "1000 1100 1200 1300 1400 1500 1600 1700 1800 1900 2000 2100"
-#define USB_LEVELS "460 500 600 700 800 900 1000"
-#define WIRELESS_LEVELS "650 800 900 1000 1100 1200"
+#define USB_LEVELS "400 500 600 700 800 900 1000 1100 1200 1300 1400 1500 1600"
+#define WIRELESS_LEVELS "600 700 800 900 1000 1100 1200 1300 1400 1500 1600"
 #define ANY_LEVELS "0 to 2100"
 
 extern int force_fast_charge;
 
-#define FAST_CHARGE_DISABLED 0	/* default */
+#define FAST_CHARGE_DISABLED 0
 #define FAST_CHARGE_FORCE_AC 1
-#define FAST_CHARGE_FORCE_CUSTOM_MA 2
+#define FAST_CHARGE_FORCE_CUSTOM_MA 2	/* default */
 
 extern int use_mtp_during_fast_charge;
 
@@ -50,29 +52,40 @@ extern int ac_charge_level;
 #define AC_CHARGE_1500 1500
 #define AC_CHARGE_1600 1600
 #define AC_CHARGE_1700 1700
-#define AC_CHARGE_1800 1800	/* default */
+#define AC_CHARGE_1800 1800
 #define AC_CHARGE_1900 1900
-#define AC_CHARGE_2000 2000
+#define AC_CHARGE_2000 2000	/* default */
 #define AC_CHARGE_2100 2100
 
 extern int usb_charge_level;
 
-#define USB_CHARGE_460 460	/* default */
+#define USB_CHARGE_400 400
 #define USB_CHARGE_500 500
 #define USB_CHARGE_600 600
-#define USB_CHARGE_700 700
+#define USB_CHARGE_700 700	/* default */
 #define USB_CHARGE_800 800
 #define USB_CHARGE_900 900
 #define USB_CHARGE_1000 1000
+#define USB_CHARGE_1100 1100
+#define USB_CHARGE_1200 1200
+#define USB_CHARGE_1300 1300
+#define USB_CHARGE_1400 1400
+#define USB_CHARGE_1500 1500
+#define USB_CHARGE_1600 1600
 
 extern int wireless_charge_level;
 
-#define WIRELESS_CHARGE_650 650	/* default */
+#define WIRELESS_CHARGE_600 600
+#define WIRELESS_CHARGE_700 700
 #define WIRELESS_CHARGE_800 800
-#define WIRELESS_CHARGE_900 900
+#define WIRELESS_CHARGE_900 900	/* default */
 #define WIRELESS_CHARGE_1000 1000
 #define WIRELESS_CHARGE_1100 1100
 #define WIRELESS_CHARGE_1200 1200
+#define WIRELESS_CHARGE_1300 1300
+#define WIRELESS_CHARGE_1400 1400
+#define WIRELESS_CHARGE_1500 1500
+#define WIRELESS_CHARGE_1600 1600
 
 #define MAX_CHARGE_LEVEL 2100	/* Whatever happens, this is the limit */
 
