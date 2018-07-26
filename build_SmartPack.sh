403aee9ad25c9355736b16a4735b82184b345d36
sunilpaulmathew <sunil.kde@gmail.com>
2018-07-26 16:16:56 +0900

    build_SmartPack: add version tag to ramdisk to access from Kernel Manager
    Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

--- build_SmartPack.sh
+++ build_SmartPack.sh
@@ -136,6 +136,8 @@ elif [ -e arch/arm/configs/$KERNEL_DEFCONFIG ]; then
 				mv -f $DTB $ANYKERNEL_DIR/dtb
 			fi
 		fi
+		# adding version tag to ramdisk in order to access from the Kernel Manager
+		echo $KERNEL_VERSION > $ANYKERNEL_DIR/ramdisk/version
 		echo -e $COLOR_GREEN"\n generating recovery flashable zip file\n"$COLOR_NEUTRAL
 		cd $ANYKERNEL_DIR && zip -r9 $KERNEL_NAME-$KERNEL_VARIANT-$KERNEL_VERSION-$KERNEL_DATE.zip * -x README.md $KERNEL_NAME-$KERNEL_VARIANT-$KERNEL_VERSION-$KERNEL_DATE.zip && cd ..
 		echo -e $COLOR_GREEN"\n cleaning...\n"$COLOR_NEUTRAL
@@ -146,6 +148,9 @@ elif [ -e arch/arm/configs/$KERNEL_DEFCONFIG ]; then
 		rm $ANYKERNEL_DIR/zImage && mv $ANYKERNEL_DIR/$KERNEL_NAME* $RELEASE_DIR
 		if [ -f $ANYKERNEL_DIR/dtb ]; then
 			rm -f $ANYKERNEL_DIR/dtb
+		fi
+		if [ -f $ANYKERNEL_DIR/ramdisk/version ]; then
+			rm -f $ANYKERNEL_DIR/ramdisk/version
 		fi
 		if [ "y" == "$PREPARE_RELEASE" ]; then
 			echo -e $COLOR_GREEN"\n Preparing for kernel release\n"$COLOR_NEUTRAL
@@ -196,6 +201,8 @@ elif [ "all" == "$KERNEL_VARIANT" ]; then
 				mv -f output_kltekor/arch/arm/boot/dt.img $ANYKERNEL_DIR/dtb
 			fi
 		fi
+		# adding version tag to ramdisk in order to access from the Kernel Manager
+		echo $KERNEL_VERSION > $ANYKERNEL_DIR/ramdisk/version
 		echo -e $COLOR_GREEN"\n generating recovery flashable zip file\n"$COLOR_NEUTRAL
 		cd $ANYKERNEL_DIR && zip -r9 $KERNEL_NAME-kltekor-$KERNEL_VERSION-$KERNEL_DATE.zip * -x README.md $KERNEL_NAME-kltekor-$KERNEL_VERSION-$KERNEL_DATE.zip && cd ..
 		echo -e $COLOR_GREEN"\n cleaning...\n"$COLOR_NEUTRAL
@@ -206,6 +213,9 @@ elif [ "all" == "$KERNEL_VARIANT" ]; then
 		rm $ANYKERNEL_DIR/zImage && mv $ANYKERNEL_DIR/$KERNEL_NAME* $RELEASE_DIR
 		if [ -f $ANYKERNEL_DIR/dtb ]; then
 			rm -f $ANYKERNEL_DIR/dtb
+		fi
+		if [ -f $ANYKERNEL_DIR/ramdisk/version ]; then
+			rm -f $ANYKERNEL_DIR/ramdisk/version
 		fi
 		echo -e $COLOR_GREEN"\n Preparing for kernel release\n"$COLOR_NEUTRAL
 		cp $RELEASE_DIR/$KERNEL_NAME-kltekor-$KERNEL_VERSION-$KERNEL_DATE.zip kernel-release/$KERNEL_NAME-kltekor.zip
@@ -251,6 +261,8 @@ elif [ "all" == "$KERNEL_VARIANT" ]; then
 				mv -f output_klte/arch/arm/boot/dt.img $ANYKERNEL_DIR/dtb
 			fi
 		fi
+		# adding version tag to ramdisk in order to access from the Kernel Manager
+		echo $KERNEL_VERSION > $ANYKERNEL_DIR/ramdisk/version
 		echo -e $COLOR_GREEN"\n generating recovery flashable zip file\n"$COLOR_NEUTRAL
 		cd $ANYKERNEL_DIR && zip -r9 $KERNEL_NAME-klte-$KERNEL_VERSION-$KERNEL_DATE.zip * -x README.md $KERNEL_NAME-klte-$KERNEL_VERSION-$KERNEL_DATE.zip && cd ..
 		echo -e $COLOR_GREEN"\n cleaning...\n"$COLOR_NEUTRAL
@@ -263,6 +275,9 @@ elif [ "all" == "$KERNEL_VARIANT" ]; then
 	else
 		if [ -f $ANYKERNEL_DIR/dtb ]; then
 			rm -f $ANYKERNEL_DIR/dtb
+		fi
+		if [ -f $ANYKERNEL_DIR/ramdisk/version ]; then
+			rm -f $ANYKERNEL_DIR/ramdisk/version
 		fi
 		echo -e $COLOR_RED"\n Building for klte failed. Please fix the issues and try again...\n"$COLOR_NEUTRAL
 	fi
@@ -302,6 +317,8 @@ elif [ "all" == "$KERNEL_VARIANT" ]; then
 				mv -f output_kltedv/arch/arm/boot/dt.img $ANYKERNEL_DIR/dtb
 			fi
 		fi
+		# adding version tag to ramdisk in order to access from the Kernel Manager
+		echo $KERNEL_VERSION > $ANYKERNEL_DIR/ramdisk/version
 		echo -e $COLOR_GREEN"\n generating recovery flashable zip file\n"$COLOR_NEUTRAL
 		cd $ANYKERNEL_DIR && zip -r9 $KERNEL_NAME-kltedv-$KERNEL_VERSION-$KERNEL_DATE.zip * -x README.md $KERNEL_NAME-kltedv-$KERNEL_VERSION-$KERNEL_DATE.zip && cd ..
 		echo -e $COLOR_GREEN"\n cleaning...\n"$COLOR_NEUTRAL
@@ -314,6 +331,9 @@ elif [ "all" == "$KERNEL_VARIANT" ]; then
 	else
 		if [ -f $ANYKERNEL_DIR/dtb ]; then
 			rm -f $ANYKERNEL_DIR/dtb
+		fi
+		if [ -f $ANYKERNEL_DIR/ramdisk/version ]; then
+			rm -f $ANYKERNEL_DIR/ramdisk/version
 		fi
 		echo -e $COLOR_RED"\n Building for kltedv failed. Please fix the issues and try again...\n"$COLOR_NEUTRAL
 	fi
@@ -353,6 +373,8 @@ elif [ "all" == "$KERNEL_VARIANT" ]; then
 				mv -f output_klteduos/arch/arm/boot/dt.img $ANYKERNEL_DIR/dtb
 			fi
 		fi
+		# adding version tag to ramdisk in order to access from the Kernel Manager
+		echo $KERNEL_VERSION > $ANYKERNEL_DIR/ramdisk/version
 		echo -e $COLOR_GREEN"\n generating recovery flashable zip file\n"$COLOR_NEUTRAL
 		cd $ANYKERNEL_DIR && zip -r9 $KERNEL_NAME-klteduos-$KERNEL_VERSION-$KERNEL_DATE.zip * -x README.md $KERNEL_NAME-klteduos-$KERNEL_VERSION-$KERNEL_DATE.zip && cd ..
 		echo -e $COLOR_GREEN"\n cleaning...\n"$COLOR_NEUTRAL
@@ -365,6 +387,9 @@ elif [ "all" == "$KERNEL_VARIANT" ]; then
 	else
 		if [ -f $ANYKERNEL_DIR/dtb ]; then
 			rm -f $ANYKERNEL_DIR/dtb
+		fi
+		if [ -f $ANYKERNEL_DIR/ramdisk/version ]; then
+			rm -f $ANYKERNEL_DIR/ramdisk/version
 		fi
 		echo -e $COLOR_RED"\n Building for klteduos failed. Please fix the issues and try again...\n"$COLOR_NEUTRAL
 	fi
@@ -404,6 +429,8 @@ elif [ "all" == "$KERNEL_VARIANT" ]; then
 				mv -f output_kltekdi/arch/arm/boot/dt.img $ANYKERNEL_DIR/dtb
 			fi
 		fi
+		# adding version tag to ramdisk in order to access from the Kernel Manager
+		echo $KERNEL_VERSION > $ANYKERNEL_DIR/ramdisk/version
 		echo -e $COLOR_GREEN"\n generating recovery flashable zip file\n"$COLOR_NEUTRAL
 		cd $ANYKERNEL_DIR && zip -r9 $KERNEL_NAME-kltekdi-$KERNEL_VERSION-$KERNEL_DATE.zip * -x README.md $KERNEL_NAME-kltekdi-$KERNEL_VERSION-$KERNEL_DATE.zip && cd ..
 		echo -e $COLOR_GREEN"\n cleaning...\n"$COLOR_NEUTRAL
@@ -411,12 +438,18 @@ elif [ "all" == "$KERNEL_VARIANT" ]; then
 		if [ -f $ANYKERNEL_DIR/dtb ]; then
 			rm -f $ANYKERNEL_DIR/dtb
 		fi
+		if [ -f $ANYKERNEL_DIR/ramdisk/version ]; then
+			rm -f $ANYKERNEL_DIR/ramdisk/version
+		fi
 		echo -e $COLOR_GREEN"\n Preparing for kernel release\n"$COLOR_NEUTRAL
 		cp $RELEASE_DIR/$KERNEL_NAME-kltekdi-$KERNEL_VERSION-$KERNEL_DATE.zip kernel-release/$KERNEL_NAME-kltekdi.zip
 		echo -e $COLOR_GREEN"\n everything done... please visit '$RELEASE_DIR'...\n"$COLOR_NEUTRAL
 	else
 		if [ -f $ANYKERNEL_DIR/dtb ]; then
 			rm -f $ANYKERNEL_DIR/dtb
+		fi
+		if [ -f $ANYKERNEL_DIR/ramdisk/version ]; then
+			rm -f $ANYKERNEL_DIR/ramdisk/version
 		fi
 		echo -e $COLOR_RED"\n Building for kltekdi failed. Please fix the issues and try again...\n"$COLOR_NEUTRAL
 	fi
